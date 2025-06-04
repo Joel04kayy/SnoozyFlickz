0344f625f09186a2b6aecc274cf6ec91562fe9fd
joel04kayy <jkayyala@uoguelph.ca>
2025-06-03 23:43:19 -0400

    Adjust title position and reduce spacing between title and wheel

--- snoozy-flickz/src/App.jsx
+++ snoozy-flickz/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import ScrollWheelArc from './components/ScrollWheelArc';
 import StarryBackground from './components/StarryBackground';
 import Logo3D from './components/Logo3D';
 import ThreeScrollWheel from './components/ThreeScrollWheel';
@@ -8,10 +7,9 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative">
       <StarryBackground />
-      <ScrollWheelArc />
       <div className="flex-1 flex flex-col items-center justify-center w-full">
         <Logo3D />
-        <div style={{ marginTop: '3rem', width: '100%' }}>
+        <div style={{ marginTop: '1rem', width: '100%' }}>
           <ThreeScrollWheel />
         </div>
       </div>
