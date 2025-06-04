5c87bad4810f5324c393bc544c08183d6362d182
joel04kayy <jkayyala@uoguelph.ca>
2025-06-03 23:55:16 -0400

    Remove spinning wheel component and its dependencies

--- snoozy-flickz/src/App.jsx
+++ snoozy-flickz/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import StarryBackground from './components/StarryBackground';
 import Logo3D from './components/Logo3D';
-import ThreeScrollWheel from './components/ThreeScrollWheel';
 import Navbar from './components/Navbar';
 import PhotoGallery from './components/PhotoGallery';
 
@@ -12,9 +11,6 @@ function App() {
       <Navbar />
       <div className="flex-1 flex flex-col items-center justify-center w-full">
         <Logo3D />
-        <div style={{ marginTop: '1rem', width: '100%' }}>
-          <ThreeScrollWheel />
-        </div>
       </div>
       <PhotoGallery />
     </div>
